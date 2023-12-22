6a10020791d289517f39ce615263b4abec701807
Aleks B <aleksandir@me.com>
2023-12-23 00:19:09 +1030

    Refactor main function to scrape multiple pages and write results to CSV

--- main.py
+++ main.py
@@ -14,34 +14,63 @@ class Book:
 
 
 def main():
-    URL = "http://books.toscrape.com"
+    for i in range(1, max_page + 1):
+        URL = f"http://books.toscrape.com/catalogue/page-{i}.html"
+        get_books(URL)
+
+    with open("books.csv", "w") as file:
+        file.write("Title,Price,Rating,Link\n")
+        for book in books:
+            file.write(f"{book.title},{book.price},{book.rating},{URL}/{book.link}\n")
+
+
+def get_max_page(URL):
+    """
+    Retrieves the maximum page number from a given URL.
+
+    Args:
+        URL (str): The URL to scrape.
+
+    Returns:
+        int: The maximum page number found on the webpage.
+    """
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, "html.parser")
+    soup = soup.find(class_="pager").find("li").get_text()
+    return int(soup[-2:])
 
+
+def get_books(URL):
+    """
+    Retrieves a list of books from the given URL.
+
+    Parameters:
+    URL (str): The URL of the webpage containing the books.
+
+    Returns:
+    list: A list of Book objects, each representing a book with its link, title, price, and rating.
+    """
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
     soup = soup.find_all(class_="product_pod")
 
     books = []
     for book in soup:
-        # <a href="catalogue/the-boys-in-the-boat-nine-americans-and-their-epic-quest-for-gold-at-the-1936-berlin-olympics_992/index.html"><img src="media/cache/66/88////66883b91f6804b2323c8369331cb7dd1.jpg" alt="The Boys in the Boat: Nine Americans and Their Epic Quest for Gold at the 1936 Berlin Olympics" class="thumbnail"></a>
+        # get link
         link = book.find("a").get("href")
 
-        # <img src="media/cache/66/88/66883b91f6804b2323c8369331cb7dd1.jpg" alt="The Boys in the Boat: Nine Americans and Their Epic Quest for Gold at the 1936 Berlin Olympics" class="thumbnail">
+        # get title
         title = book.find("h3").find("a")["title"]
         title = title.replace(",", "")
 
-        # <div class="product_price">
-        # <p class="price_color">£22.60</p>
+        # get price
         price = book.find(class_="price_color").get_text()
 
         # <p class="star-rating Three">
         rating = book.find(class_="star-rating")["class"][1]
         books.append(Book(link, title, price, rating))
 
-    with open("books.csv", "w") as file:
-        file.write("Title,Price,Rating,Link\n")
-        for book in books:
-            file.write(f"{book.title},{book.price},{book.rating},{URL}/{book.link}\n")
+    return books
 
 
 if __name__ == "__main__":
